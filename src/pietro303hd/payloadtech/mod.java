fe5287469d3c9aea9b9c777af8e8b0c8bf238d67
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-08-15 14:03:29 -0300

    Create mod.java

---
+++ src/pietro303hd/payloadtech/mod.java
@@ -0,0 +1,14 @@
+package pietro303hd.payloadtech;
+
+import mindustry.mod.Mod;
+
+public class ModTemplate extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
